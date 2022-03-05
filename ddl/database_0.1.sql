c5eecdb50d30c87857c09cf3e24ff55cd4408939
Giuseppe <83022686+M3lo00@users.noreply.github.com>
2022-03-05 11:57:50 +0100

    databseFinito

--- ddl/database_0.1.sql
+++ ddl/database_0.1.sql
@@ -1,3 +1,4 @@
+USE dbproj;
 DROP TABLE IF EXISTS `audit`;
 DROP TABLE IF EXISTS `chosenoptional`;
 DROP TABLE IF EXISTS `employee`;
@@ -11,36 +12,114 @@ DROP TABLE IF EXISTS `service`;
 DROP TABLE IF EXISTS `servpack`;
 DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `audit` (
-  `refUser` 			int NOT NULL,
-  `refLastRejection` 	int NOT NULL,
-  
-  PRIMARY KEY (`refUser`,`refLastRejection`),
-  CONSTRAINT `fk_Payment` FOREIGN KEY (`refLastRejection`) REFERENCES `payment` (`idPayments`),
-  CONSTRAINT `fk_User2` 
-	FOREIGN KEY (`refUser`) REFERENCES `user` (`idUser`)
-)
+CREATE TABLE `user` (
+    `idUser` 			int 		NOT NULL AUTO_INCREMENT,
+    `email` 			varchar(45) NOT NULL,
+    `Username` 			varchar(45) NOT NULL,
+    `Password` 			varchar(45) NOT NULL,
+    `Insolvent` 		tinyint(3) 	unsigned DEFAULT 0 NOT NULL,
+    `failedPay` 		int(10) 	unsigned DEFAULT 0 NOT NULL,
 
-CREATE TABLE `chosenoptional` (
-  `refOrder` 			int NOT NULL,
-  `refOptService` 		int NOT NULL,
-  
-  PRIMARY KEY (`refOrder`,`refOptService`),
-  KEY `fk_Optional_idx` (`refOptService`), /* these are indices which may or may not be needed*/
-  CONSTRAINT `fk_Optional` 
-	FOREIGN KEY (`refOptService`) REFERENCES `optservice` (`idOptService`),
-  CONSTRAINT `fk_Order` 
-	FOREIGN KEY (`refOrder`) REFERENCES `order` (`idOrder`)
-)
+    PRIMARY KEY (`idUser`),
+    UNIQUE `Username_UNIQUE` (`Username`),
+    UNIQUE `email_UNIQUE` (`email`)
+);
 
 CREATE TABLE `employee` (
-  `idEmployee` 			int NOT NULL AUTO_INCREMENT,
-  `email` 				varchar(45) NOT NULL,
-  `password` 			varchar(45) NOT NULL,
-  
-  PRIMARY KEY (`idEmployee`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-)
+    `idEmployee` 		int NOT NULL AUTO_INCREMENT,
+    `email` 			varchar(45) NOT NULL,
+    `password` 			varchar(45) NOT NULL,
+
+    PRIMARY KEY (`idEmployee`),
+    UNIQUE KEY `email_UNIQUE` (`email`)
+);
+
+CREATE TABLE `service` (
+    `idService` 		int NOT NULL AUTO_INCREMENT,
+    `Type` 				varchar(45) NOT NULL,
+
+    PRIMARY KEY (`idService`)
+);
+
+CREATE TABLE `package` (
+    `idPackage`         int NOT NULL AUTO_INCREMENT,
+    `name`              varchar(45) NOT NULL,
+    `refEmployee`       int DEFAULT NULL, /*forse sarebbe meglio mettere un employee 0 che crea i pacchetti "iniziali"*/
+
+    PRIMARY KEY (`idPackage`),
+    UNIQUE KEY `name_UNIQUE` (`name`),
+    /*KEY `fk_EmployeePack_idx` (`refEmployee`),*/
+    CONSTRAINT `fk_EmployeePack`
+        FOREIGN KEY (`refEmployee`) REFERENCES `employee` (`idEmployee`)
+);
+
+CREATE TABLE `order` (
+     `idOrder` 			int NOT NULL AUTO_INCREMENT,
+     `refUser` 			int NOT NULL,
+     `refPack` 			int NOT NULL,
+     `period` 			int NOT NULL,
+     `creationDate` 	datetime NOT NULL,
+     `startDate` 		date DEFAULT NULL,
+     `valid` 			tinyint(3) unsigned DEFAULT 0 NOT NULL,
+     `totalAmount` 		int NOT NULL,
+
+     PRIMARY KEY (`idOrder`),
+     CONSTRAINT `fk_Package3`
+         FOREIGN KEY (`refPack`) REFERENCES `package` (`idPackage`),
+     CONSTRAINT `fk_User`
+         FOREIGN KEY (`refUser`) REFERENCES `user` (`idUser`)
+);
+
+CREATE TABLE `payment` (
+    `idPayments` 			int NOT NULL,
+    `refOrder` 			int NOT NULL,
+    `refUser` 			int NOT NULL,
+    `status` 				tinyint NOT NULL,
+    `payTime` 			datetime NOT NULL,
+
+    PRIMARY KEY (`idPayments`),
+    CONSTRAINT `fk_Order1`
+       FOREIGN KEY (`refOrder`) REFERENCES `order` (`idOrder`),
+    CONSTRAINT `fk_User1`
+       FOREIGN KEY (`refUser`) REFERENCES `user` (`idUser`)
+);
+
+CREATE TABLE `audit` (
+    `refUser` 			int NOT NULL,
+    `refLastRejection` 	int NOT NULL,
+
+    PRIMARY KEY (`refUser`,`refLastRejection`),
+    CONSTRAINT `fk_Payment`
+        FOREIGN KEY (`refLastRejection`) REFERENCES `payment` (`idPayments`),
+    CONSTRAINT `fk_User2`
+        FOREIGN KEY (`refUser`) REFERENCES `user` (`idUser`)
+);
+
+CREATE TABLE `optservice` (
+      `idOptService` 		int NOT NULL,
+      `name` 				varchar(45) NOT NULL,
+      `monthly` 			int NOT NULL,
+      `refEmployee` 		int DEFAULT NULL,
+
+      PRIMARY KEY (`idOptService`),
+      UNIQUE KEY `Name_UNIQUE` (`name`),
+    /*KEY `fk_Employee_idx` (`refEmployee`), same as before*/
+      CONSTRAINT `fk_Employee`
+          FOREIGN KEY (`refEmployee`) REFERENCES `employee` (`idEmployee`)
+);
+
+CREATE TABLE `chosenoptional` (
+        `refOrder` 			int NOT NULL,
+        `refOptService` 		int NOT NULL,
+
+        PRIMARY KEY (`refOrder`,`refOptService`),
+        /* KEY `fk_Optional_idx` (`refOptService`),  these are indices which may or may not be needed*/
+        CONSTRAINT `fk_Optional`
+            FOREIGN KEY (`refOptService`) REFERENCES `optservice` (`idOptService`),
+        CONSTRAINT `fk_Order`
+            FOREIGN KEY (`refOrder`) REFERENCES `order` (`idOrder`)
+);
+
 
 CREATE TABLE `feeperiod` (
   `refPackage` 			int NOT NULL,
@@ -50,37 +129,7 @@ CREATE TABLE `feeperiod` (
   PRIMARY KEY (`refPackage`,`period`),
   CONSTRAINT `fk_Package2` 
 	FOREIGN KEY (`refPackage`) REFERENCES `package` (`idPackage`)
-)
-
-CREATE TABLE `optservice` (
-  `idOptService` 		int NOT NULL,
-  `name` 				varchar(45) NOT NULL,
-  `monthly` 			int NOT NULL,
-  `refEmployee` 		int DEFAULT NULL,
-  
-  PRIMARY KEY (`idOptService`),
-  UNIQUE KEY `Name_UNIQUE` (`name`),
-  KEY `fk_Employee_idx` (`refEmployee`), /*same as before*/
-  CONSTRAINT `fk_Employee` 
-	FOREIGN KEY (`refEmployee`) REFERENCES `employee` (`idEmployee`)
-)
-
-CREATE TABLE `order` (
-  `idOrder` 			int NOT NULL AUTO_INCREMENT,
-  `refUser` 			int NOT NULL,
-  `refPack` 			int NOT NULL,
-  `period` 				int NOT NULL,
-  `creationDate` 		datetime NOT NULL,
-  `startDate` 			date DEFAULT NULL,
-  `valid` 				tinyint(3) unsigned DEFAULT 0 NOT NULL,
-  `totalAmount` 		int NOT NULL,
-  
-  PRIMARY KEY (`idOrder`),
-  CONSTRAINT `fk_Package3` 
-	FOREIGN KEY (`refPack`) REFERENCES `package` (`idPackage`),
-  CONSTRAINT `fk_User` 
-	FOREIGN KEY (`refUser`) REFERENCES `user` (`idUser`)
-)
+);
 
 CREATE TABLE `ownoptservice` (
   `refServPack` 		int NOT NULL,
@@ -91,28 +140,7 @@ CREATE TABLE `ownoptservice` (
 	FOREIGN KEY (`refOptService`) REFERENCES `optservice` (`idOptService`),
   CONSTRAINT `fk_Package` 
 	FOREIGN KEY (`refServPack`) REFERENCES `package` (`idPackage`)
-)
-
-CREATE TABLE `payment` (
-  `idPayments` 			int NOT NULL,
-  `refOrder` 			int NOT NULL,
-  `refUser` 			int NOT NULL,
-  `status` 				tinyint NOT NULL,
-  `payTime` 			datetime NOT NULL,
-  
-  PRIMARY KEY (`idPayments`),
-  CONSTRAINT `fk_Order1` 
-	FOREIGN KEY (`refOrder`) REFERENCES `order` (`idOrder`),
-  CONSTRAINT `fk_User1` 
-	FOREIGN KEY (`refUser`) REFERENCES `user` (`idUser`)
-)
-
-CREATE TABLE `service` (
-  `idService` 			int NOT NULL AUTO_INCREMENT,
-  `Type` 				varchar(45) NOT NULL,
-  
-  PRIMARY KEY (`idService`)
-)
+);
 
 CREATE TABLE `servpack` (
   `refService` 			int NOT NULL,
@@ -128,31 +156,6 @@ CREATE TABLE `servpack` (
 	FOREIGN KEY (`refPackage`) REFERENCES `package` (`idPackage`),
   CONSTRAINT `fk_Service` 
 	FOREIGN KEY (`refService`) REFERENCES `service` (`idService`)
-)
+);
 
-CREATE TABLE `user` (
-  `idUser` 				int NOT NULL AUTO_INCREMENT,
-  `email` 				varchar(45) NOT NULL,
-  `Username` 			varchar(45) NOT NULL,
-  `Password` 			varchar(45) NOT NULL,
-  `Insolvent` 			tinyint(3) unsigned zerofill DEFAULT NULL,
-  `failedPay` 			int(10) unsigned zerofill DEFAULT NULL,
-  
-  PRIMARY KEY (`idUser`),
-  UNIQUE KEY `Username_UNIQUE` (`Username`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-)
-
-CREATE TABLE `user` (
-  `idUser` 				int 		NOT NULL AUTO_INCREMENT,
-  `email` 				varchar(45) NOT NULL,
-  `Username` 			varchar(45) NOT NULL,
-  `Password` 			varchar(45) NOT NULL,
-  `Insolvent` 			tinyint(3) 	unsigned DEFAULT 0 NOT NULL,
-  `failedPay` 			int(10) 	unsigned DEFAULT 0 NOT NULL,
-  
-  PRIMARY KEY (`idUser`),
-  UNIQUE `Username_UNIQUE` (`Username`),
-  UNIQUE `email_UNIQUE` (`email`)
-)
 

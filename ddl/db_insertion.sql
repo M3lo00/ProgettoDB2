f01d5ec64ace86a4372407621c3006786ccd80ea
Giuseppe <83022686+M3lo00@users.noreply.github.com>
2022-03-05 12:07:07 +0100

    OK

---
+++ ddl/db_insertion.sql
@@ -0,0 +1,9 @@
+USE dbproj;
+
+INSERT INTO service (Type) VALUES ('Fixed Phone');
+INSERT INTO service (Type) VALUES ('Mobile Phone');
+INSERT INTO service (Type) VALUES ('Fixed Internet');
+INSERT INTO service (Type) VALUES ('Mobile Internet');
+
+INSERT INTO `user` (`username`, `password`, `email`) VALUES ('a', 'a', 'a');
+INSERT INTO `employee` (`username`, `password`, `email`) VALUES ('e', 'e', 'e');

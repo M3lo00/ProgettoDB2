654552caf416222148a539489b99c4d671c3ac53
Giuseppe <giusmeli2000@gmail.com>
2022-03-07 16:17:47 +0100

    bo

--- ddl/db_insertion.sql
+++ ddl/db_insertion.sql
@@ -5,5 +5,5 @@ INSERT INTO service (Type) VALUES ('Mobile Phone');
 INSERT INTO service (Type) VALUES ('Fixed Internet');
 INSERT INTO service (Type) VALUES ('Mobile Internet');
 
-INSERT INTO 'user' ('username', 'password', 'email') VALUES ('giuseppe', 'Peppe2098', 'giuseppe@polimi.it');
-INSERT INTO 'employee' ('password', 'email') VALUES ('Peppe2098', 'giuseppe.employee@polimi.it');
+INSERT INTO user (username, password, email) VALUES ('giuseppe', 'Peppe2098', 'giuseppe@polimi.it');
+INSERT INTO employee (password, email) VALUES ('Peppe2098', 'giuseppe.employee@polimi.it');

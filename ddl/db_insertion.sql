942ba861c0a4ac1b2bbe25958fc88b69b8edde6d
Giuseppe <giusmeli2000@gmail.com>
2022-03-09 15:46:25 +0100

    ownoptservice update (fixed)

--- ddl/db_insertion.sql
+++
@@ -1,9 +0,0 @@
-USE dbproj;
-
-INSERT INTO service (Type) VALUES ('Fixed Phone');
-INSERT INTO service (Type) VALUES ('Mobile Phone');
-INSERT INTO service (Type) VALUES ('Fixed Internet');
-INSERT INTO service (Type) VALUES ('Mobile Internet');
-
-INSERT INTO user (username, password, email) VALUES ('giuseppe', 'Peppe2098', 'giuseppe@polimi.it');
-INSERT INTO employee (password, email) VALUES ('Peppe2098', 'giuseppe.employee@polimi.it');

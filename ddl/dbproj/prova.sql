8788850c6b24037cc8b35d5db5b8c99bc198310b
giuseppegiovia123 <86290456+giuseppegiovia123@users.noreply.github.com>
2022-03-10 01:01:15 +0100

    prova trigger 4

--- ddl/dbproj/prova.sql
+++ ddl/dbproj/prova.sql
@@ -1,4 +1,5 @@
-SELECT o.idOrder, count(*)
+DECLARE x int
+SELECT o.idOrder, count(*) INTO x
 FROM dbproj.order as o
     JOIN chosenoptional c on o.idOrder = c.refOrder
     JOIN ownoptservice o2 on o.refPack = o2.refPack
--- ddl/trigger4.sql
+++ ddl/trigger4.sql
@@ -1,3 +1,4 @@
+USE dbproj;
 create table avgproductperservice
 (
     package_id          int not null primary key ,
@@ -20,64 +21,23 @@ END;
 CREATE TRIGGER addOrderAvg
     AFTER INSERT ON `order` for each row
 BEGIN
-    DECLARE temporaryChos, temporaryOptOwn  int;
+    DECLARE temporaryChos  int;
     IF NEW.valid = 1 THEN
-        SELECT c.refOptService, count(*)
-        FROM 'order' as o
+        SELECT count(*), o.idOrder INTO temporaryChos
+        FROM dbproj.order as o
+                 JOIN chosenoptional c on o.idOrder = c.refOrder
+                 JOIN ownoptservice o2 on o.refPack = o2.refPack
+        WHERE o.refPack = NEW.refPack
+        GROUP BY o.idOrder;
+
+
         UPDATE avgproductperservice
-        SET numoptservice = numoptservice + (temporaryCho+temporaryOptOwn),
-            justPack = justPack + ( SELECT f.fee
-                                    FROM feeperiod f
-                                    WHERE f.refPackage=NEW.refPack and f.periodo=NEW.periodo)
+        SET numoptservice = numoptservice + temporaryChos,
+            numpackage = numpackage +1
         WHERE package_id =NEW.refPack;
     END IF;
 END;
 
-
-
-CREATE DEFINER  = CURRENT_USER trigger addOptProduct
-    after insert
-    on `order`
-    for each row
-begin
-
-    IF NEW.isValid = true THEN
-        DELETE FROM totalnumberofoptionalproduct t
-        WHERE t.package_id IN (SELECT s.packageSelected
-                               FROM servicepackage s
-                               WHERE s.servicePackage_id = NEW.servicePackageAssociated );
-
-
-        INSERT INTO totalnumberofoptionalproduct
-        SELECT ss.servicePackageToSelect_id, count(*)
-        FROM `order` as o
-                 JOIN dbtelco.servicepackage as s on o.servicePackageAssociated = s.servicePackage_id
-                 JOIN dbtelco.servicepackagetoselect as ss on s.packageSelected = ss.servicePackageToSelect_id
-                 JOIN dbtelco.addedproduct as a on a.servicePackage_id = o.servicePackageAssociated
-        WHERE o.isValid = true AND ss.servicePackageToSelect_id IN (SELECT s.packageSelected
-                                                                    FROM servicepackage s
-                                                                    WHERE s.servicePackage_id = NEW.servicePackageAssociated)
-
-        GROUP BY ss.servicePackageToSelect_id;
-
-
-        DELETE FROM avgnumofoptproductssoldperpackage
-        WHERE package_id IN (   SELECT s.packageSelected
-                                FROM servicepackage s
-                                WHERE s.servicePackage_id = NEW.servicePackageAssociated);
-
-        INSERT INTO avgnumofoptproductssoldperpackage
-        SELECT t.package_id, IFNULL((o.total / t.totalPurchases), 0.0)
-        FROM dbtelco.totalpurchasesperpackage as t
-                 LEFT OUTER JOIN dbtelco.totalnumberofoptionalproduct as o on t.package_id = o.package_id
-
-        WHERE t.package_id IN (SELECT s.packageSelected
-                               FROM servicepackage s
-                               WHERE s.servicePackage_id = NEW.servicePackageAssociated);
-
-
-    END IF;
-end
 
 
 

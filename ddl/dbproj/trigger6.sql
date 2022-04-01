d06a8bb795524bbc9de8b924d96b09e254527eda
Giuseppe <83022686+M3lo00@users.noreply.github.com>
2022-04-01 12:41:17 +0200

    Finito? Forse! Controlla le cose da fare

--- ddl/dbproj/trigger6.sql
+++ ddl/dbproj/trigger6.sql
@@ -2,6 +2,8 @@ create trigger addToTotalValue
     AFTER INSERT on chosenoptional for each row
 BEGIN
     UPDATE optservice
-    SET totSold = totSold + optservice.monthly
+    SET totSold = totSold + (optservice.monthly * ( SELECT o.periodo
+                                                    FROM `order` AS o
+                                                    WHERE o.idOrder=NEW.refOrder))
     WHERE idOptService = NEW.refOptService;
 end;
